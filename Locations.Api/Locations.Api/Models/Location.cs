a885370ecf5420e42dcdb7bdea8f6da9b6ab6175
jamh <jamh26@users.noreply.github.com>
2022-04-12 20:29:44 -0600

    feat: initial creation of new api project
    added Location model

---
+++ Locations.Api/Locations.Api/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locations.Api.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}

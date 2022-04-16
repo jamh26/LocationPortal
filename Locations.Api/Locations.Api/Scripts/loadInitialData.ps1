2650e7d717a193e7034d479bb5bf30f10bcab4a1
jamh <jamh26@users.noreply.github.com>
2022-04-15 18:43:16 -0600

    chore: added scripts to load data and adjusted launchsettings

---
+++ Locations.Api/Locations.Api/Scripts/loadInitialData.ps1
@@ -0,0 +1,22 @@
+﻿$filepath = "$PSScriptRoot/dataLoad.csv"
+
+Import-Csv $filepath |
+ForEach-Object {
+    $Body = @{
+     physicalAddress = $_.physicalAddress
+     zipCode = $_.zipCode
+     city = $_.city
+     }
+     $JsonBody = $Body | ConvertTo-Json
+
+    # The ContentType will automatically be set to application/x-www-form-urlencoded for
+    # all POST requests, unless specified otherwise.
+     $Params = @{
+         Method = "Post"
+         Uri = "https://localhost:44318/api/locations"
+         Body = $JsonBody
+         ContentType = "application/json"
+     }
+     Invoke-RestMethod @Params
+}
+

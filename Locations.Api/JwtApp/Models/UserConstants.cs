27f1c15442e76b476a0b511b0d4460e3bf25aa50
jamh <jamh26@users.noreply.github.com>
2022-04-17 11:21:04 -0600

    feat: added test login API endpoints

---
+++ Locations.Api/JwtApp/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { UserName = "admin", EmailAddress = "admin@local.com", Password = "password", GivenName = "Jacob", Surname = "Admin", Role = "Admin" },
+            new UserModel() { UserName = "user", EmailAddress = "user@local.com", Password = "password", GivenName = "James", Surname = "User", Role = "User" }
+        };
+    }
+}
